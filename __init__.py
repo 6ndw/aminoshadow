dc6821ba65e68da44721ca60c4df431f06731350
6ndw <109902358+6ndw@users.noreply.github.com>
2025-01-30 13:36:13 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,8 +13,8 @@ from .socket import Callbacks, SocketHandler
 from requests import get
 from json import loads
 
-__newest__ = loads(get("https://pypi.org/pypi/amino.fix/json").text)["info"]["version"]
+__newest__ = loads(get("https://pypi.org/pypi/shadowamino/json").text)["info"]["version"]
 
 if __version__ != __newest__:
     print(f"New version of {__title__} available: {__newest__} (Using {__version__})")
-    print("Visit our discord - https://discord.gg/Bf3dpBRJHj")
+    print("Visit our community - http://aminoapps.com/c/Utaou")
