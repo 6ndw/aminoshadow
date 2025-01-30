282a7102a29762dae29e28a4db6c37ad4e6eef78
6ndw <109902358+6ndw@users.noreply.github.com>
2025-01-30 12:43:07 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shadowamino',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='tmhat2374@gmail.com',
+    description='aminoapps lib for python',
+    url='https://instagram.com/po60',  # Replace with your actual GitHub repo (optional)
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
